207451e968372b5ba9e3d6a5c41313aebb3791e7
caofangyu <caofangyu1997@163.com>
2023-09-15 01:14:10 +0800

    to: add sync tcp client and server

--- Net/asio/tcp_sync_server.cc
+++ Net/asio/tcp_sync_server.cc
@@ -9,38 +9,26 @@
 using namespace boost::asio;
 using boost::system::error_code;
 io_service service;
-ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 8001);
 
-size_t read_complete(char* buf, const error_code& err, size_t bytes) {
-  if (err) return 0;
-  bool found = std::find(buf, buf + bytes, '\n') < buf + bytes;
-  // 我们一个一个读取直到读到回车，不缓存
-  return found ? 0 : 1;
+size_t read_complete(char * buff, const error_code & err, size_t bytes) {
+    if ( err) return 0;
+    bool found = std::find(buff, buff + bytes, '\n') < buff + bytes;
+    // 我们一个一个读取直到读到回车，不缓存
+    return found ? 0 : 1;
 }
-
-void sync_echo(std::string msg) {
-  msg += "\n";
-      ip::tcp::socket sock(service);
-  sock.connect(ep);
-  sock.write_some(buffer(msg));
-  char buf[1024];
-  int bytes = read(sock, buffer(buf), boost::bind(read_complete, buf, _1, _2));
-  std::string copy(buf, bytes - 1);
-  msg = msg.substr(0, msg.size() - 1);
-  std::cout << "server echoed our " << msg << ": "
-            << (copy == msg ? "OK" : "FAIL") << std::endl;
-  sock.close();
+void handle_connections() {
+    ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(),8001));
+    char buff[1024];
+    while ( true) {
+        ip::tcp::socket sock(service);
+        acceptor.accept(sock);
+        int bytes = read(sock, buffer(buff), boost::bind(read_complete,buff,_1,_2));
+        //std::string msg(buff, bytes);
+        std::string msg("hello world \n");
+        sock.write_some(buffer(msg));
+        sock.close();
+    }
 }
-
 int main(int argc, char* argv[]) {
-  char* messages[] = {"John says hi", "so does James", "Lucy just got home",
-                      "Boost.Asio is Fun!", 0};
-  boost::thread_group threads;
-  for (char** message = messages; *message; ++message) {
-    threads.create_thread(boost::bind(sync_echo, *message));
-    boost::this_thread::sleep(boost::posix_time::millisec(100));
-  }
-  threads.join_all();
-
-  system("pause");
+    handle_connections();
 }
