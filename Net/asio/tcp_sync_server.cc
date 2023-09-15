b32d94d0cf961d51fd63192f79fbbf799bb2ff83
caofy <caofy@antwork.link>
2023-09-15 18:07:10 +0800

    minor update

--- Net/asio/tcp_sync_server.cc
+++ Net/asio/tcp_sync_server.cc
@@ -5,6 +5,10 @@
 #include <boost/thread.hpp>
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <sstream>
 
 using namespace boost::asio;
 using boost::system::error_code;
@@ -19,14 +23,30 @@ size_t read_complete(char * buff, const error_code & err, size_t bytes) {
 void handle_connections() {
     ip::tcp::acceptor acceptor(service, ip::tcp::endpoint(ip::tcp::v4(),8001));
     char buff[1024];
+    ip::tcp::socket sock(service);
+    acceptor.accept(sock);
+    std::cout << "accept, read to read" << std::endl;
+    //time print
+    time_t cur;
+    struct tm *local_time;
     while ( true) {
-        ip::tcp::socket sock(service);
-        acceptor.accept(sock);
+        std::cout << "start read" << std::endl;
         int bytes = read(sock, buffer(buff), boost::bind(read_complete,buff,_1,_2));
-        //std::string msg(buff, bytes);
-        std::string msg("hello world \n");
+        std::string msg(buff, bytes);
+        std::cout << "read msg: " << msg;
         sock.write_some(buffer(msg));
-        sock.close();
+        //for (int i = 0; i < 1000; ++i) {
+        //    //time
+        //    std::stringstream ss;
+        //    time(&cur);
+        //    local_time = localtime(&cur);
+        //    ss << local_time->tm_hour << ":" 
+        //       << local_time->tm_min  << ":" 
+        //       << local_time->tm_sec;
+        //    sock.write_some(buffer(ss.str()));
+        //    std::this_thread::sleep_for(std::chrono::seconds(1));
+        //}
+        //sock.close();
     }
 }
 int main(int argc, char* argv[]) {
