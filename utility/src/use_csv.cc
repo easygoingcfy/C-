608378b78082780850cf9dafde90c788af6bb9d6
caofy <caofy@antwork.link>
2023-12-11 10:21:20 +0800

    update

--- utility/src/use_csv.cc
+++ utility/src/use_csv.cc
@@ -12,11 +12,16 @@ int main() {
     // test file exists
     std::ifstream infile(filename);
     std::ofstream outfile(filename, std::ios_base::app);
-    auto writer = csv::make_csv_writer(outfile);
+    csv::CSVWriter<std::ofstream> writer = csv::make_csv_writer(nullptr);
+    csv::CSVWriter<std::ofstream> writer = csv::make_csv_writer(outfile);
+    std::vector<std::string> column = {
+        "A", "B", "C"
+    };
     if (!infile.good()) {
-        writer << std::vector<std::string>({"A", "B", "C"});
+        writer << column;
     }
     writer << std::array<int, 3>({1, 2, 3});
     writer << std::array<int, 3>({4, 5, 6});
+    writer << std::array<int, 7>({4, 5, 6, 7, 8, 9, 10});
     return 0;
 }
